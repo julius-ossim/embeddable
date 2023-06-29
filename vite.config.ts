4c8e6857e83cbc71c9a01b4ad1bdd9a41a119620
Julius Ossim <juliusjacksonossim@gmail.com>
2023-06-29 20:30:43 +0100

    ci(): testing CI/CD

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'src/lib/index.ts'),
             name: 'atunwaPlayer',
-            formats: ['es', 'umd'],
+            formats: ['es', 'cjs'],
             fileName: (format) => `atunwa-player.${format}.js`,
         },
         rollupOptions: {
